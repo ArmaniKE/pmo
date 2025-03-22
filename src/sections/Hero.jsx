ff7ff9b6316f0e376707e5ed167dbcefa970b0a9
ArmaniKE <armani.kabidolda.18.12.03@gmail.com>
2025-03-22 16:47:13 +0500

    hero video

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Hero = () => {
+  const videoRef = useRef(null);
+
+  const mouseEnter = () => {
+    videoRef.current.play();
+  };
+
+  const mouseLeave = () => {
+    videoRef.current.pause();
+  };
+
   return (
     <div className="px-[80px] mt-[150px] flex">
       <div className="w-[50%] px-6 flex-col content-center space-y-[40px]">
         <div className="leading-6">
-          <span className="text-black text-5xl font-semibold">Управляем проектами — создаем </span>
+          <span className="text-black text-5xl font-semibold">
+            Управляем проектами — создаем{" "}
+          </span>
           <span className="text-[#002F6C] text-5xl font-semibold">успех</span>
           <span className="text-black text-5xl font-semibold">!</span>
         </div>
@@ -14,17 +26,26 @@ const Hero = () => {
           максимальной эффективностью, используя современные подходы проектного
           управления
         </div>
-        <div
-          data-property-1="Default"
-          className="w-48 h-11 p-2 bg-[#002F6C] rounded-lg inline-flex justify-center items-center overflow-hidden"
-        >
-          <div className="text-center text-white text-md font-semibold">
+        <div className="w-48 h-11 p-2 bg-[#002F6C] rounded-lg inline-flex justify-center items-center overflow-hidden">
+          <button className="text-center text-white text-md font-semibold cursor-pointer">
             Связаться с нами
-          </div>
+          </button>
         </div>
       </div>
-      <div className="w-[55%] py-10 px-6">
-        <img src="/hero/vid.png" />
+      <div className="w-[50%] py-10 px-6">
+        <video
+          ref={videoRef}
+          onMouseEnter={mouseEnter}
+          onMouseLeave={mouseLeave}
+          autoPlay
+          loop
+          playsInline
+          muted
+          className="rounded-2xl shadow-xl"
+        >
+          <source src="hero/hero.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   );
